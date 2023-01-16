778f2c2796429cf4801c0c06e9ac99e4f02b674d
Alia-creates <117888229+Alia-creates@users.noreply.github.com>
2023-01-15 23:26:05 -0800

    added windElement to app.js

--- src/app.js
+++ src/app.js
@@ -2,8 +2,15 @@ function showTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = response.data.wind.deg;
 }
 
 let key = "f135e1be3f84490782d52465398cdb5b";
