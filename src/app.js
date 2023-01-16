f0ece8399355e6115db8431d1b1fcf8ddb8a87f0
Alia-creates <117888229+Alia-creates@users.noreply.github.com>
2023-01-15 22:49:22 -0800

    updated app.js with openweather.api

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
-let query = "Los Angeles";
-let key = "93efccd922aoa89t53f9abbece2045a0";
-let units = "imperial";
-function showUser(response) {
-  alert(`The user name is ${response.data.name}`);
+function showTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
 }
 
-let url = "https://jsonplaceholder.typicode.com/users/1";
-axios.get(url).then(showUser);
+let key = "f135e1be3f84490782d52465398cdb5b";
+let apiURL =
+  "https://api.openweathermap.org/data/2.5/weather?q=Roseville&appid=f135e1be3f84490782d52465398cdb5b";
+
+axios.get(apiURL).then(showTemperature);
