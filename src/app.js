7fa6d25d577b066f33571c79c7e770ec12cde84c
Alia-creates <117888229+Alia-creates@users.noreply.github.com>
2023-01-15 20:51:27 -0800

    updated script: added query, key, units

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+let query = "Los Angeles";
+let key = "93efccd922aoa89t53f9abbece2045a0";
+let units = "imperial";
 function showUser(response) {
   alert(`The user name is ${response.data.name}`);
 }
